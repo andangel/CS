ac7d617ee7a296932961311de8bab9d01fec00c4
andangel <andangel@vip.qq.com>
2017-10-10 19:16:40 +0800

    显示接口

---
+++ A04多态显示接口/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A04多态显示接口
+{
+    /// <summary>
+    /// 显示时间接口就是为了解决方法重命名问题
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Birds:IFlyable
+    {
+        public void Flyable()
+        {
+            Console.WriteLine("鸟会飞!");
+        }
+        public void IFlyable.Flyable()
+        {
+            Console.WriteLine("我是接口的飞!");
+        }
+        
+    }
+    public interface IFlyable
+    {
+        public void Flyable();
+    }
+}
