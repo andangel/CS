5c3ab7c28e83b7683f097e68e6745efb6d3c357e
andangel <andangel@vip.qq.com>
2017-10-10 19:04:34 +0800

    接口练习

---
+++ A03多态接口/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A03多态接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperMan superman = new SuperMan();
+            superman.飞行();
+
+        }
+    }
+}
---
+++ 多态接口练习一/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 多态接口练习一
+{
+    /// <summary>
+    /// 麻雀会飞 鹦鹉会飞 鸵鸟不会飞 企鹅不会飞 直升飞机会飞
+    ///
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Birds[] bird = new Birds [10];
+            Random r = new Random();
+            for (int i = 0; i < bird.Length; i++)
+            {
+                int rNumber = r.Next(1,4);
+                switch (rNumber)
+                 {
+                    case 1: bird[i]=new Sparrow();
+                        break;
+                    case 2: bird[i] = new Penguin();
+                        break;
+                    case 3:bird[i] = new Parrot();
+                        break;
+                    case 4:bird[i] = new Ostrich();
+                        break;
+                }
+            }
+            for (int i = 0; i < bird.Length; i++)
+            {
+                if (bird[i] is Sparrow)
+                {
+                    ((Sparrow)bird[i]).Flyable();
+                }
+                else if (bird[i] is Penguin)
+                {
+                    ((Penguin)bird[i]).Flyable();
+                }
+                else if (bird[i] is Parrot)
+                {
+                     ((Parrot)bird[i]).Flyable();
+                }
+                else if (bird[i] is Ostrich)
+                {
+                    ((Ostrich)bird[i]).Flyable();
+                }
+            }
+        }
+    }
+}
