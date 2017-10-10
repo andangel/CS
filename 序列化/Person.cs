f9a3d8e914c12b0f44ccaf0ee02125637a575267
andangel <andangel@vip.qq.com>
2017-10-10 17:03:46 +0800

    序列化

---
+++ 序列化/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 序列化
+{
+    /// <summary>
+    /// Serializable 可序列化类标识
+    /// </summary>
+    [Serializable]
+    class Person
+    {
+        private string _name;
+        private int _age;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age 
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public Person() { }
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        public Person(string name, int age)
+            : this(name)
+        {
+            this.Age = age;
+        }
+    }
+}
---
+++ 序列化/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace 序列化
+{
+    /// <summary>
+    /// 将对象转换为二进制
+    /// 作用:传输数据
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("zh", 100);
+            //序列化一个对象
+            using (FileStream fsWrite = new FileStream(@"./序列化.txt", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fsWrite, p);
+            }
+            //反序列化
+            Person pp;
+            using (FileStream fsRead=new FileStream(@"./序列化.txt", FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                pp = (Person)bf.Deserialize(fsRead);
+            }
+            Console.WriteLine(pp.Name);
+            Console.WriteLine(pp.Age);
+        }
+    }
+}
