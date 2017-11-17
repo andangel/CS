c75db05823b33345d6f6ac4bf1d18428e37c013c
andangel <andangel@vip.qq.com>
2017-11-17 15:12:31 +0800

    单例模式有bug

---
+++ 单例模式/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 单例模式
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form frm2 = new Form2();
+            Form frm2 = Form2.Getsingle();
+            frm2.Show();
+        }
+    }
+}
---
+++ 单例模式/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 单例模式
+{
+    public partial class Form2 : Form
+    {
+        //全局唯一的单例字段
+        public static Form2 FrmSingle=null;
+
+        //public Form2() 将构造函数私有化
+        private Form2()
+        {
+            InitializeComponent();
+        }
+        //创建窗口的静态方法
+        public static Form2 Getsingle()
+        {
+           // Form2 frm2 = new Form2();
+            //return frm2;
+            if (FrmSingle == null)//有bug 第二次创建程序崩溃
+            {
+                FrmSingle = new Form2();
+            }
+            return FrmSingle;
+        }
+    }
+}
