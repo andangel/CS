d9ef0ad6642521b522692e163f39463d8ae4070e
andangel <andangel@vip.qq.com>
2017-11-17 15:21:00 +0800

    解决单例模式 无法访问已释放的对象。BUG

--- 单例模式/Form2.cs
+++ 单例模式/Form2.cs
@@ -24,7 +24,8 @@ namespace 单例模式
         {
            // Form2 frm2 = new Form2();
             //return frm2;
-            if (FrmSingle == null)//有bug 第二次创建程序崩溃
+            if (FrmSingle == null||FrmSingle.IsDisposed)//有bug 第二次创建程序崩溃
+            //bug解决思路:C#的自动回收机制。关闭窗体。它就设为IsDisposed为true，表示该窗口占用的resource已经释放了。但是这时候系统可能还没有收集。只是标识这个该收集了。至于什么时候收集就是系统决定的了。所以你去判断的时候它不为空，就不会创建新的。但是它已经把所有的资源释放了。所以这套机制就不让你去访问啊。因为它不一定什么时候就被回收了。那就是null了。 
             {
                 FrmSingle = new Form2();
             }
