bff6e5f52a2c46a9edf87bf8ddac08ec9bd3c23f
KaiKai <andangel@vip.qq.com>
2018-09-03 18:25:41 +0800

    按钮事件

---
+++ 按钮外观及事件/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace 按钮外观及事件
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(49, 73);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(393, 189);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "button1";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(49, 399);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(393, 189);
+            this.Button2.TabIndex = 1;
+            this.Button2.Text = "修改按钮";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            this.Button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button2_MouseClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 720);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1140, 41);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(105, 36);
+            this.toolStripStatusLabel1.Text = "aaaaaa";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 761);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ 按钮外观及事件/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace 按钮外观及事件
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //改变按钮1位置
+            Button1.Location = new Point(675, 499);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Text="要让字体自适应DPI只需创建一个manifest配置清单";
+            Button1.Text = "关闭";
+            Button1.Font = new Font("微软雅黑", 26, FontStyle.Bold, GraphicsUnit.Point, ((byte)(134)));
+            Button1.ForeColor = SystemColors.HotTrack;
+            Button2.Text = "开始";
+            Button2.Font = new Font("微软雅黑", 26, FontStyle.Bold, GraphicsUnit.Point, ((byte)(134)));
+            Button2.ForeColor = SystemColors.HotTrack;
+        }
+
+        private void Button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            //右键中键其他键
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("右键!");
+            }
+            toolStripStatusLabel1.Text = string.Format("X:{0},Y:{1},button:{2},Clockes:{3}",e.X.ToString(), e.Y.ToString(),e.Button,e.Clicks);
+
+        }
+    }
+}
