b7a7f1a12aa8703c1420bdd810c8e251cfde3972
KaiKai <andangel@vip.qq.com>
2018-09-03 13:02:31 +0800

    事件执行顺序

---
+++ 窗体基本事件及事件顺序/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace 窗体基本事件及事件顺序
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(71, 52);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(310, 24);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "任何控件 都有一个基本属性";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(353, 316);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(169, 53);
+            this.Button1.TabIndex = 1;
+            this.Button1.Text = "关闭";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 429);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Label1);
+            this.Name = "FormMain";
+            this.Text = "窗体事件";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ 窗体基本事件及事件顺序/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 窗体基本事件及事件顺序
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            //初始化窗体基本信息
+            //如果将代码卸载Init方法后,可能引发事件顺序错乱
+            InitializeComponent();
+
+
+            //如果在面板里删除Load事件可以通过手动绑定事件如下
+            //this.Load+=FormMain_Load;
+            //代码自动生成的Load事件是带有Private
+            //代码自动生成的事件绑定在InitializeComponent()内.
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //窗体第一次被初始化.(窗体标题,窗体大小,是否允许最大化,最小化)
+            this.Text = "这是由Load事件修改的窗体标题";
+        }
+        //使用任务管理器对软件结束时,不会触发改事件,因为事件只能处理程序内部消息
+        
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //后
+            //可以利用断点来分析顺序
+            //执行完Closing后再执行此事件
+            //
+            MessageBox.Show("Closed事件", "2", MessageBoxButtons.OK);
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //前
+            //当我点击事件按钮时,先执行此事件.
+            //如果需要执行清理.销毁对象,代码卸载此处更合适.
+            MessageBox.Show("Closing事件","1", MessageBoxButtons.OK);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Close()先执行Closing事件再执行Closed事件
+            this.Close();
+        }
+
+        private void FormMain_SizeChanged(object sender, EventArgs e)
+        {
+            //窗体修改事件
+            //this.Width = 600;
+            //this.Height =500;
+            this.Text = string.Format("当前窗体宽:{0},高:{1}", this.Width,this.Height);
+        }
+    }
+}
